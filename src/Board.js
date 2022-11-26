070e362082bfab95e7837b7d011f168de6df0017
jabbott1999 <jabbott1999@gmail.com>
2022-11-26 01:18:08 +0000

    Finish base functionality

--- src/Board.js
+++ src/Board.js
@@ -8,7 +8,7 @@ import React, {
 } from "react";
 
 const Board = forwardRef((props, ref) => {
-  const size = 6;
+  const size = 25;
 
   const [board, setBoard] = useState();
   const [solvedSquares, setSolvedSquares] = useState([{ col: 0, row: 0 }]);
@@ -28,24 +28,78 @@ const Board = forwardRef((props, ref) => {
     const newBoard = [];
     board.forEach((row) => newBoard.push([...row]));
 
+    const newSolvedSquares = [...solvedSquares];
+
     // Set current solved squares to a color
-    solvedSquares.forEach((s) => {
+    newSolvedSquares.forEach((s) => {
       newBoard[s.col][s.row] = createSquare(color);
     });
 
+    const newColor = props.colors[color];
+
     // Add new solved squares
-    solvedSquares.forEach((s) => {
-      if (
-        s.col + 1 < size &&
-        getSquareColor(newBoard[s.col + 1][s.row]) === props.colors[color]
-      ) {
-        console.log("HELLO");
-        setSolvedSquares([]);
-        console.log(solvedSquares);
-      }
-    });
+    newSolvedSquares.forEach((square) =>
+      updateLocalSolvedSquares(square, newBoard, newSolvedSquares, newColor)
+    );
 
     return newBoard;
+  };
+
+  const updateLocalSolvedSquares = (
+    s,
+    newBoard,
+    newSolvedSquares,
+    newColor
+  ) => {
+    if (
+      s.col + 1 < size &&
+      isSquareSolved(newSolvedSquares, s.col + 1, s.row) &&
+      getSquareColor(newBoard[s.col + 1][s.row]) === newColor
+    ) {
+      const newSquare = addSolvedSquare(newSolvedSquares, s.col + 1, s.row);
+      updateLocalSolvedSquares(newSquare, newBoard, newSolvedSquares, newColor);
+    }
+
+    if (
+      s.row + 1 < size &&
+      isSquareSolved(newSolvedSquares, s.col, s.row + 1) &&
+      getSquareColor(newBoard[s.col][s.row + 1]) === newColor
+    ) {
+      const newSquare = addSolvedSquare(newSolvedSquares, s.col, s.row + 1);
+      updateLocalSolvedSquares(newSquare, newBoard, newSolvedSquares, newColor);
+    }
+
+    if (
+      s.col - 1 > -1 &&
+      isSquareSolved(newSolvedSquares, s.col - 1, s.row) &&
+      getSquareColor(newBoard[s.col - 1][s.row]) === newColor
+    ) {
+      const newSquare = addSolvedSquare(newSolvedSquares, s.col - 1, s.row);
+      updateLocalSolvedSquares(newSquare, newBoard, newSolvedSquares, newColor);
+    }
+
+    if (
+      s.row - 1 > -1 &&
+      isSquareSolved(newSolvedSquares, s.col, s.row - 1) &&
+      getSquareColor(newBoard[s.col][s.row - 1]) === newColor
+    ) {
+      const newSquare = addSolvedSquare(newSolvedSquares, s.col, s.row - 1);
+      updateLocalSolvedSquares(newSquare, newBoard, newSolvedSquares, newColor);
+    }
+
+    setSolvedSquares(newSolvedSquares);
+  };
+
+  const isSquareSolved = (solvedSquares, col, row) => {
+    return !solvedSquares.find((s) => s.row === row && s.col === col);
+  };
+
+  const addSolvedSquare = (solvedSquares, col, row) => {
+    if (isSquareSolved(solvedSquares, col, row)) {
+      solvedSquares.push({ col, row });
+    }
+
+    return { col, row };
   };
 
   const createSquare = (color) => {
